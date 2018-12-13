76304cd0aa0085dcfb8f913c10b432df119fc8bc
신예지 <gjsld1@naver.com>
2018-12-14 00:06:55 +0900

    jihye 추가

--- app/src/main/java/com/team/dbgo/MypokemonActivity.kt
+++ app/src/main/java/com/team/dbgo/MypokemonActivity.kt
@@ -1,24 +1,22 @@
 package com.team.dbgo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import retrofit2.Call
 import retrofit2.Response
-import android.widget.Button
+import kotlinx.android.synthetic.main.activity_mypokemon.*
+import kotlinx.android.synthetic.main.my_pokenmons.*
+import kotlinx.android.synthetic.main.myitems.*
 
 
-class MypokemonActivity : AppCompatActivity(), View.OnClickListener {
-    private var mypokeList : RecyclerView? = null
+class MypokemonActivity : AppCompatActivity() {
+    private var adapter: MypokemonAdapter? = null
     private var mypokeDatas : ArrayList<MypokemonData>? = null
-    private var adapter : MypokemonAdapter? = null
 
     private var networkService: NetworkService? = null
     private var requestManager: RequestManager? = null
@@ -30,18 +28,47 @@ class MypokemonActivity : AppCompatActivity(), View.OnClickListener {
         networkService = ApplicationController.instance!!.networkService
         requestManager = Glide.with(this)
 
-        mypokeList = findViewById(R.id.poke_list) as RecyclerView
-        mypokeList!!.layoutManager = LinearLayoutManager(this)
+        poke_list.layoutManager = LinearLayoutManager(this)
 
         val getPokemonInfoResponse = networkService!!.getPokemonInfo()
         getPokemonInfoResponse.enqueue(object : retrofit2.Callback<PokemonInfoResponse>{
             override fun onResponse(call: Call<PokemonInfoResponse>?, response: Response<PokemonInfoResponse>?) {
                 if(response!!.isSuccessful){
                     if(response!!.body().pokemons != null){
-                        adapter = MypokemonAdapter(response!!.body().pokemons !!, requestManager!!)
+                        adapter = MypokemonAdapter(response!!.body().pokemons !!, requestManager!!) { position ->
+                            val seq : Int? = mypokeDatas!!.get(position).seq
+                            val name : String? = mypokeDatas!!.get(position).name
+                            val cp : String? = mypokeDatas!!.get(position).cp.toString()
+                            val hp : Int? = mypokeDatas!!.get(position).hp
+                            val weight : String? = mypokeDatas!!.get(position).weight
+                            val height : String? = mypokeDatas!!.get(position).height
+                            val level : Int? = mypokeDatas!!.get(position).level
+                            val catched_at : String? = mypokeDatas!!.get(position).catched_at
+                            val normal_attack_name : String? = mypokeDatas!!.get(position).normal_attack_name
+                            val specail_attack_name : String? = mypokeDatas!!.get(position).specail_attack_name
+
+                            //Toast.makeText(this@MypokemonActivity, name, Toast.LENGTH_SHORT).show()
+                            var intent = Intent(this@MypokemonActivity, PokemonDetailActivity::class.java)
+                            intent.putExtra("name", name)
+                            intent.putExtra("cp",cp)
+                            intent.putExtra("weight", weight)
+                            intent.putExtra("height", height)
+                            intent.putExtra("catched_at", catched_at)
+
+                            startActivity(intent)
+//                            evolveButton.setOnClickListener { _ ->
+//                                val getEvolvePokemonResponse = networkService!!.getEvolvePokemon()
+//                                getEvolvePokemonResponse.enqueue(object : retrofit2.Callback<EvolvePokemonResponse> {
+//                                    override fun onResponse(call: Call<EvolvePokemonResponse>?, response: Response<EvolvePokemonResponse>?) {
+//                                        if (response!!.isSuccessful) {
+//
+//                                        }
+//                                    }
+//                                })
+//                            }
+                        }
                         mypokeDatas = response!!.body().pokemons
-                        adapter!!.setOnItemClickListener(this@MypokemonActivity)
-                        mypokeList!!.adapter = adapter
+                        poke_list.adapter = adapter
                     }
                 }
             }
@@ -49,27 +76,6 @@ class MypokemonActivity : AppCompatActivity(), View.OnClickListener {
                 ApplicationController.instance!!.makeToast("통신 확인")
             }
         })
-
-//        val getEvolvePokemonResponse = networkService!!.getEvolvePokemon()
-////        getEvolvePokemonResponse.enqueue(object : retrofit2.Callback<EvolvePokemonResponse>{
-////            override fun onResponse(call: Call<EvolvePokemonResponse>?, response: Response<EvolvePokemonResponse>?) {
-////                if(response!!.isSuccessful){
-////
-////                }
-////            }
-////            override fun onFailure(call: Call<EvolvePokemonResponse>?, t: Throwable?) {
-////                ApplicationController.instance!!.makeToast("통신 확인")
-////            }
-////        })
-    }
-    override fun onClick(v: View?) {
-
-        Log.v("yjjh", "yjjh")
-        val idk : Int = mypokeList!!.getChildAdapterPosition(v!!)
-        Log.v("jh", "jh")
-        val name : String? = mypokeDatas!!.get(idk).name
-        Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
-
 
     }
 }
--- app/src/main/java/com/team/dbgo/MypokemonAdapter.kt
+++ app/src/main/java/com/team/dbgo/MypokemonAdapter.kt
@@ -1,18 +1,31 @@
 package com.team.dbgo
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.RequestManager
 
-class MypokemonAdapter(var dataList : ArrayList<MypokemonData>, var requestManager: RequestManager): RecyclerView.Adapter<MypokemonViewHolder>() {
+/**
+ * 아이템 클릭 리스너
+ */
+fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
+    itemView.setOnClickListener {
+        event.invoke(adapterPosition, itemViewType)
+    }
+    return this
+}
 
-    private var onItemClick : View.OnClickListener? = null
+
+class MypokemonAdapter(var dataList : ArrayList<MypokemonData>, var requestManager: RequestManager, val onClicked: (position: Int) -> Unit): RecyclerView.Adapter<MypokemonViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): MypokemonViewHolder {
         val mainView : View = LayoutInflater.from(parent.context).inflate(R.layout.my_pokenmons, parent, false)
-        return MypokemonViewHolder(mainView)
+        return MypokemonViewHolder(mainView).listen { position, _ ->
+            Log.d("adsf", "$position")
+            onClicked(position)
+        }
     }
 
     override fun onBindViewHolder(holder: MypokemonViewHolder, position:  Int) {
@@ -20,8 +33,4 @@ class MypokemonAdapter(var dataList : ArrayList<MypokemonData>, var requestManag
     }
 
     override fun getItemCount() : Int = dataList!!.size
-
-    fun setOnItemClickListener(l:View.OnClickListener){
-        onItemClick = l
-    }
 }
